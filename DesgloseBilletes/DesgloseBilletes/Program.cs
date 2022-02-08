ae99908f1a3291691693e201dda4e9877a5907f1
JADP <jadelarosapaulino@gmail.com>
2022-02-08 01:15:16 -0400

    update

--- DesgloseBilletes/DesgloseBilletes/Program.cs
+++ DesgloseBilletes/DesgloseBilletes/Program.cs
@@ -15,49 +15,23 @@ namespace DesgloseBilletes
 
             var amount = int.Parse(Console.ReadLine());
 
-            int a = amount, billete = 0, btotal = 0;
+            int a = amount, btotal = 0;
 
 
             foreach (var num in bList)
             {
-                //if (num >= a)
-                //{
-                //billete = num;                    
-                //for (int i = 0; i < a; i++)
-                //{
-                //    btotal += 1;
-                //}
-
-                //Console.WriteLine(billete + " x " + btotal + " = " + billete * btotal);
-                //foreach (var item in collection)
-                //{
-
-                //}
                 if (a >= num)
                 {
-                    int i;
-                    for (i = 0; num <= a; i++)
+                    for (int i = 0; num <= a; i++)
                     {
                         btotal = btotal + 1;
                         a = a - num;
                     }
-                    
-                    Console.WriteLine(num + " x " + btotal);
-                } else
-                {
-                    //continue;
+                    Console.WriteLine(btotal + " x " +  num + " = " + btotal * num);
                 }
-                    
 
-                
-
-                    //return;
-                //}
-
-                //Console.WriteLine(num);
+                btotal = 0;
             }
-
-
 
             Console.ReadLine();
         }
